9bf56d2b839b3fa8784fa9f222e75e6c6ec9f0f5
lpm <leodanypm@gmail.com>
2025-11-04 22:19:54 -0500

    Proyecto Finalizado

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,30 +4,43 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # 2
-df['overweight'] = None
+df['overweight'] = df.apply(lambda x:1 if ((x['weight']*10000)/(x['height']**2))>25 else 0, axis=1)
 
 # 3
-
-
+df['cholesterol'] = df['cholesterol'].apply(lambda x:1 if x>1 else 0 )
+df['gluc'] = df['gluc'].apply(lambda x:1 if x>1 else 0 )
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
+    df_cat = df_cat = pd.melt(
+    df,                           
+    id_vars=['cardio'],                
+    value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'], 
+    var_name='variable',                
+    value_name='value'                  
+    )
 
 
     # 6
-    df_cat = None
+    df_cat = df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='total')
     
 
     # 7
-
+    graf = sns.catplot(
+    data=df_cat, 
+    x='variable', 
+    y='total', 
+    hue='value', 
+    col='cardio', 
+    kind='bar'
+    ).fig
 
 
     # 8
-    fig = None
+    fig = graf
 
 
     # 9
@@ -38,21 +51,32 @@ def draw_cat_plot():
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
-
+    df_heat = df[(df['ap_lo'] <= df['ap_hi']) &
+    (df['height'] >= df['height'].quantile(0.025)) &
+    (df['height'] <= df['height'].quantile(0.975)) &
+    (df['weight'] >= df['weight'].quantile(0.025)) &
+    (df['weight'] <= df['weight'].quantile(0.975))]
     # 12
-    corr = None
+    corr = df_heat.corr()
 
     # 13
-    mask = None
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
 
 
     # 14
-    fig, ax = None
-
+    fig, ax = plt.subplots(figsize=(12, 10))
+    
     # 15
-
+    sns.heatmap(corr, 
+            mask=mask,        
+            annot=True,
+            cmap='coolwarm',
+            center=0,
+            fmt='.1f',
+            square=True,
+            ax=ax)
+    plt.show()
 
 
     # 16
